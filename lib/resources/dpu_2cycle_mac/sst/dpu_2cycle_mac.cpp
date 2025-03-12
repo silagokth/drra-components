59641af06c7a81b98c7217e8f5d8774d54a8c32e
Edwared Yang <herenvarno@live.com>
2025-03-12 15:17:29 +0100

    update 2cycle dpu

--- lib/resources/dpu_2cycle_mac/sst/dpu_2cycle_mac.cpp
+++ lib/resources/dpu_2cycle_mac/sst/dpu_2cycle_mac.cpp
@@ -118,7 +118,25 @@ void DPU2CycleMac::handleRep(uint32_t instr) {
   }
 }
 
-void DPU2CycleMac::handleRepx(uint32_t instr) { handleRep(instr); }
+void DPU2CycleMac::handleRepx(uint32_t instr) {
+  // Instruction fields
+  uint32_t port = getInstrField(instr, 2, 22);
+  uint32_t level = getInstrField(instr, 4, 18);
+  uint32_t iter_msb = getInstrField(instr, 6, 12);
+  uint32_t step_msb = getInstrField(instr, 6, 6);
+  uint32_t delay_msb = getInstrField(instr, 6, 0);
+
+  auto repetition_op =
+      next_timing_states[0].getRepetitionOperatorFromLevel(level);
+  uint32_t iter = iter_msb << 6 | repetition_op.getIterations();
+  uint32_t step = step_msb << 6 | repetition_op.getStep();
+  uint32_t delay = delay_msb << 6 | repetition_op.getDelay();
+  try {
+    next_timing_states[0].adjustRepetition(iter, delay, level, step);
+  } catch (const std::exception &e) {
+    out.fatal(CALL_INFO, -1, "REPX failed: %s\n", e.what());
+  }
+}
 
 void DPU2CycleMac::handleFSM(uint32_t instr) {
   // Instruction fields
