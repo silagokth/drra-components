852440319d5025a442dc7e6b42abf2dd1f4b502f
Paul Delestrac <paul.delestrac@gmail.com>
2025-03-26 18:24:26 +0100

    Revert "added rule to cmake to copy folder with a rtl subfolder"
    This reverts commit 823c98fe29ad519031bb9def92b145a5261a6493.

--- cmake/modules/FindAddComponent.cmake
+++ cmake/modules/FindAddComponent.cmake
@@ -106,20 +106,7 @@ function(add_all_subdirs)
   subdirlist(SUBDIRS ${CMAKE_CURRENT_SOURCE_DIR})
 
   foreach(subdir ${SUBDIRS})
-    if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${subdir}/CMakeLists.txt")
-      add_subdirectory(${subdir})
-    endif()
-
-    if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${subdir}/rtl")
-      set(CMAKE_COMPONENTS_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/library/common/${subdir})
-
-      # Copy Bender file
-      file(GLOB BENDER_FILE "Bender.yml")
-      file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/${subdir}/${BENDER_FILE} DESTINATION ${CMAKE_COMPONENTS_OUTPUT_DIRECTORY})
-
-      file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/${subdir}/rtl DESTINATION ${CMAKE_COMPONENTS_OUTPUT_DIRECTORY})
-    endif()
-
+    add_subdirectory(${subdir})
     set(full_path ${CMAKE_CURRENT_SOURCE_DIR}/${subdir})
 
     if(USE_SST)
