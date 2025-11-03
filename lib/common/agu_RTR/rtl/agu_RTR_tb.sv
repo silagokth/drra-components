129c76897421c7a95056ca6b1c6104db8f4dc870
Paul Delestrac <paul.delestrac@gmail.com>
2025-11-03 15:58:36 +0100

    chore(agu_RTR): formatted controller; modified tb

--- lib/common/agu_RTR/rtl/agu_RTR_tb.sv
+++ lib/common/agu_RTR/rtl/agu_RTR_tb.sv
@@ -9,9 +9,9 @@ module agu_RTR_tb #(
     localparam TRANS_LEVEL_WIDTH = 4,
     localparam TRANS_DELAY_WIDTH = 12,
 
-    localparam NUMBER_OR = 4,  // OR: Outter R-Pattern
-    localparam NUMBER_MT = 3,  // MT: Middle T-Pattern
-    localparam NUMBER_IR = 4   // IR: Inner  R-Pattern
+    localparam NUMBER_OR = 2,  // OR: Outter R-Pattern
+    localparam NUMBER_MT = 1,  // MT: Middle T-Pattern
+    localparam NUMBER_IR = 2   // IR: Inner  R-Pattern
 ) ();
 
   logic                         clk;
@@ -98,248 +98,45 @@ module agu_RTR_tb #(
     /////////////////////////////////// IR != 0 => RTR, TR, RR, R ///////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////// Delay == 0
 
-
     // IR0
+    @(posedge clk);
     rep_valid = 1'b1;
-    rep_step_IR[0] = 3;
+    rep_step_IR[0] = 1;
     rep_delay_IR[0] = 0;
-    rep_iter_IR[0] = 2;
+    rep_iter_IR[0] = 6'h3f;
     rep_config_IR[0] = 1'b1;  // L0, S3,  D0, iter2
-    rep_step_IR[1] = 1;
-    rep_delay_IR[1] = 0;
-    rep_iter_IR[1] = 3;
-    rep_config_IR[1] = 1'b1;  // L1, S1,  D0, iter3
-    rep_step_IR[2] = 2;
-    rep_delay_IR[2] = 0;
-    rep_iter_IR[2] = 1;
-    rep_config_IR[2] = 1'b1;  // L2, S2,  D0, iter1
-    rep_step_IR[3] = 10;
-    rep_delay_IR[3] = 0;
-    rep_iter_IR[3] = 2;
-    rep_config_IR[3] = 1'b1;  // L3, S10, D0, iter2
 
-    // MT0
-    trans_valid = 1'b1;
-    trans_delay[0] = 0;
-    trans_config[0] = 1'b1;
+    @(posedge clk);
+    rep_valid = 1'b0;
+    rep_step_IR[0] = 0;
+    rep_delay_IR[0] = 0;
+    rep_iter_IR[0] = '0;
+    rep_config_IR[0] = 1'b0;  // L0, S3,  D0, iter2
 
     // IR1
-    rep_step_IR[4] = 2;
-    rep_delay_IR[4] = 0;
-    rep_iter_IR[4] = 2;
-    rep_config_IR[4] = 1'b1;  // L0, S2, D0, iter2
-    rep_step_IR[5] = 3;
-    rep_delay_IR[5] = 0;
-    rep_iter_IR[5] = 3;
-    rep_config_IR[5] = 1'b1;  // L1, S3, D0, iter3
-    rep_step_IR[6] = 5;
-    rep_delay_IR[6] = 0;
-    rep_iter_IR[6] = 1;
-    rep_config_IR[6] = 1'b1;  // L2, S5, D0, iter1
-    rep_step_IR[7] = 4;
-    rep_delay_IR[7] = 0;
-    rep_iter_IR[7] = 1;
-    rep_config_IR[7] = 1'b1;  // L3, S4, D0, iter1
+    @(posedge clk);
+    @(posedge clk);
+    @(posedge clk);
+    rep_valid = 1'b1;
+    rep_step_IR[1] = 0;
+    rep_delay_IR[1] = 0;
+    rep_iter_IR[1] = 7;
+    rep_config_IR[1] = 1'b1;  // L1, S1,  D0, iter3
 
-    // MT1
-    trans_delay[1] = 0;
-    trans_config[1] = 1'b1;
+    @(posedge clk);
+    rep_valid = 1'b0;
+    rep_step_IR[0] = 0;
+    rep_delay_IR[0] = 0;
+    rep_iter_IR[0] = '0;
+    rep_config_IR[0] = 1'b0;  // L0, S3,  D0, iter2
 
-    // IR2
-    rep_step_IR[8] = 7;
-    rep_delay_IR[8] = 0;
-    rep_iter_IR[8] = 1;
-    rep_config_IR[8] = 1'b1;  // L0, S7, D0, iter1
-    rep_step_IR[9] = 1;
-    rep_delay_IR[9] = 0;
-    rep_iter_IR[9] = 2;
-    rep_config_IR[9] = 1'b1;  // L1, S1, D0, iter2
-    rep_step_IR[10] = 3;
-    rep_delay_IR[10] = 0;
-    rep_iter_IR[10] = 3;
-    rep_config_IR[10] = 1'b1;  // L2, S3, D0, iter3
-    rep_step_IR[11] = 5;
-    rep_delay_IR[11] = 0;
-    rep_iter_IR[11] = 2;
-    rep_config_IR[11] = 1'b1;  // L3, S5, D0, iter2
-
-    // MT2
-    trans_delay[2] = 0;
-    trans_config[2] = 1'b1;
-
-    // IR3
-    rep_step_IR[12] = 1;
-    rep_delay_IR[12] = 0;
-    rep_iter_IR[12] = 5;
-    rep_config_IR[12] = 1'b1;  // L0, S1, D0, iter5
-    rep_step_IR[13] = 6;
-    rep_delay_IR[13] = 0;
-    rep_iter_IR[13] = 1;
-    rep_config_IR[13] = 1'b1;  // L1, S6, D0, iter1
-    rep_step_IR[14] = 0;
-    rep_delay_IR[14] = 0;
-    rep_iter_IR[14] = 2;
-    rep_config_IR[14] = 1'b1;  // L2, S0, D0, iter2
-    rep_step_IR[15] = 5;
-    rep_delay_IR[15] = 0;
-    rep_iter_IR[15] = 1;
-    rep_config_IR[15] = 1'b1;  // L3, S5, D0, iter1
-
-    // OR
-    rep_step_OR[0] = 10;
-    rep_delay_OR[0] = 0;
-    rep_iter_OR[0] = 2;
-    rep_config_OR[0] = 1'b1;  // L4, S10, D0, iter2
-    rep_step_OR[1] = 3;
-    rep_delay_OR[1] = 0;
-    rep_iter_OR[1] = 1;
-    rep_config_OR[1] = 1'b1;  // L5, S3, D0, iter1
-    rep_step_OR[2] = 5;
-    rep_delay_OR[2] = 0;
-    rep_iter_OR[2] = 1;
-    rep_config_OR[2] = 1'b1;  // L6, S5, D0, iter1
-    rep_step_OR[3] = 1;
-    rep_delay_OR[3] = 0;
-    rep_iter_OR[3] = 1;
-    rep_config_OR[3] = 1'b1;  // L7, S1, D0, iter1
-
-    ////////////////////////////////////////////////////////////////////////////////////// Delay != 0
-    // // Must be issued one cycle before the first rep instruction, to go to the CNFG state
-    // @(posedge clk);
-    // rep_valid = 1'b1;
-
-    // // IR0
-    // @(posedge clk);
-    // rep_level = 0; rep_step = 3;  rep_delay = 2; rep_iter = 2; // L0, S3,  D2, iter2
-    // @(posedge clk);  
-    // rep_level = 1; rep_step = 1;  rep_delay = 1; rep_iter = 3; // L1, S1,  D1, iter3
-    // @(posedge clk);  
-    // rep_level = 2; rep_step = 2;  rep_delay = 3; rep_iter = 1; // L2, S2,  D3, iter1
-    // @(posedge clk);
-    // rep_level = 3; rep_step = 10; rep_delay = 0; rep_iter = 2; // L3, S10, D0, iter2
-
-    // // MT0
-    // @(posedge clk);
-    // rep_valid = 1'b0;
-    // trans_valid = 1'b1;
-    // trans_level = 0; trans_delay = 7;
-
-    // // IR1
-    // @(posedge clk);
-    // rep_valid = 1'b1;
-    // trans_valid = 1'b0;
-    // rep_level = 0; rep_step = 2; rep_delay = 0; rep_iter = 2; // L0, S2, D0, iter2
-    // @(posedge clk);
-    // rep_level = 1; rep_step = 3; rep_delay = 0; rep_iter = 3; // L1, S3, D0, iter3
-    // @(posedge clk);
-    // rep_level = 2; rep_step = 5; rep_delay = 0; rep_iter = 1; // L2, S5, D0, iter1
-    // @(posedge clk);
-    // rep_level = 3; rep_step = 4; rep_delay = 3; rep_iter = 1; // L3, S4, D3, iter1
-
-    // // MT1
-    // @(posedge clk);
-    // rep_valid = 1'b0;
-    // trans_valid = 1'b1;
-    // trans_level = 1; trans_delay = 3;
-
-    // // IR2
-    // @(posedge clk);
-    // rep_valid = 1'b1;
-    // trans_valid = 1'b0;
-    // rep_level = 0; rep_step = 7; rep_delay = 0; rep_iter = 1; // L0, S7, D0, iter1
-    // @(posedge clk);
-    // rep_level = 1; rep_step = 1; rep_delay = 0; rep_iter = 2; // L1, S1, D0, iter2
-    // @(posedge clk);
-    // rep_level = 2; rep_step = 3; rep_delay = 0; rep_iter = 3; // L2, S3, D0, iter3
-    // @(posedge clk);
-    // rep_level = 3; rep_step = 5; rep_delay = 0; rep_iter = 2; // L3, S5, D0, iter2
-
-    // // MT2
-    // @(posedge clk);
-    // rep_valid = 1'b0;
-    // trans_valid = 1'b1;
-    // trans_level = 2; trans_delay = 0;
-
-    // // IR3
-    // @(posedge clk);
-    // rep_valid = 1'b1;
-    // trans_valid = 1'b0;
-    // rep_level = 0; rep_step = 1; rep_delay = 1; rep_iter = 5; // L0, S1, D1, iter5
-    // @(posedge clk);
-    // rep_level = 1; rep_step = 6; rep_delay = 3; rep_iter = 1; // L1, S6, D3, iter1
-    // @(posedge clk);
-    // rep_level = 2; rep_step = 0; rep_delay = 7; rep_iter = 2; // L2, S0, D7, iter2
-    // @(posedge clk);
-    // rep_level = 3; rep_step = 5; rep_delay = 9; rep_iter = 1; // L3, S5, D9, iter1
-
-    // // OR
-    // @(posedge clk);
-    // rep_level = 4; rep_step = 10; rep_delay = 0; rep_iter = 2; // L4, S10, D0, iter2
-    // @(posedge clk);
-    // rep_level = 5; rep_step = 3;  rep_delay = 5; rep_iter = 1; // L5, S3,  D5, iter1
-    // @(posedge clk); 
-    // rep_level = 6; rep_step = 5;  rep_delay = 0; rep_iter = 1; // L6, S5,  D0, iter1
-    // @(posedge clk); 
-    // rep_level = 7; rep_step = 1;  rep_delay = 9; rep_iter = 1; // L7, S1,  D9, iter1
-
-
-    ////////////////////////////////////// IR == 0 => RT, T, 0 //////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////// Delay == 0
-    // Must be issued one cycle before the first rep instruction, to go to the CNFG state
-    // @(posedge clk);
-    // trans_valid = 1'b1;
-
-    // // MT0
-    // @(posedge clk);
-    // trans_level = 0; trans_delay = 0;
-
-    // // MT1
-    // @(posedge clk);
-    // trans_level = 1; trans_delay = 0;
-
-    // // MT2
-    // @(posedge clk);
-    // trans_level = 2; trans_delay = 0;
-
-    // // OR
-    // @(posedge clk);
-    // rep_valid = 1'b1;
-    // trans_valid = 1'b0;
-    // rep_level = 0; rep_step = 10; rep_delay = 0; rep_iter = 2; // L0, S10, D0, iter2
-    // @(posedge clk);
-    // rep_level = 1; rep_step = 3;  rep_delay = 0; rep_iter = 1; // L1, S3,  D0, iter1
-    // @(posedge clk); 
-    // rep_level = 2; rep_step = 5;  rep_delay = 0; rep_iter = 3; // L2, S5,  D0, iter3
-    // @(posedge clk); 
-    // rep_level = 3; rep_step = 1;  rep_delay = 0; rep_iter = 1; // L3, S1,  D0, iter1
-
-    ////////////////////////////////////////////////////////////////////////////////////// Delay != 0
-    // @(posedge clk);
-    // instr_en = 1'b1;
-
-    // // trans_level_delay
-    // @(posedge clk);
-    // instr = 25'b010_0001_000000000000000011;
-    // @(posedge clk);
-    // instr = 25'b010_0000_000000000000000111;
-
-    // // rep_option_level_step_delay_iter
-    // @(posedge clk);
-    // instr = 25'b000_00_000_1010_0000010_000010; // op0, L0, S10, D0, iter2
-    // @(posedge clk);
-    // instr = 25'b000_00_001_0011_0000101_000001; // op0, L1, S3, D0, iter1
-    // @(posedge clk);
-    // instr = 25'b000_00_010_0101_0001010_000011; // op0, L2, S5, D0, iter3
-
-    ////////////////////////////////////////////////////////////////
-
-
+    @(posedge clk);
     @(posedge clk);
     activation = 1'b1;
     @(posedge clk);
     activation = 1'b0;
 
-    #64000;  //WO delay
+    #5600;  //WO delay
     $stop;
   end
 
