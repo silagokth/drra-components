7d50ca238e697bb00c0012e13c4786114da30282
Paul Delestrac <paul.delestrac@gmail.com>
2025-12-10 16:05:59 +0100

    fix(agu_RTR): cleaned up the testrun bash scripts

--- lib/common/agu_RTR/rtl/ir_top_testrun.sh
+++ lib/common/agu_RTR/rtl/ir_top_testrun.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# Use ir_top_compile.do and ir_top_sim.do to compile and simulate the IR generator testbench
 vsim -c -sv_lib ./timingModel -work work -do "do ir_top_compile.do; run -all; quit"
-# vsim -c -do "do ir_top_compile.do; do ir_top_sim.do; quit"
--- lib/common/agu_RTR/rtl/mt_ir_top_testrun.sh
+++ lib/common/agu_RTR/rtl/mt_ir_top_testrun.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# Use ir_top_compile.do and ir_top_sim.do to compile and simulate the IR generator testbench
 vsim -c -sv_lib ./timingModel -work work -do "do mt_ir_top_compile.do; run -all; quit"
